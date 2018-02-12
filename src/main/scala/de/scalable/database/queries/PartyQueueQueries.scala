0b3335265503b27d0c5ee5e72efe24faf86ca60f
griase94 <andy.griesbeck@gmail.com>
2018-02-12 17:03:45 +0100

    added some queries

--- src/main/scala/de/scalable/database/queries/PartyQueueQueries.scala
+++ src/main/scala/de/scalable/database/queries/PartyQueueQueries.scala
@@ -1,19 +1,32 @@
 package de.scalable.database.queries
 
-import de.scalable.database.ScalableDB
-import de.scalable.model.Song
+
+import java.time.LocalDateTime
+
+import de.scalable.model.{PartyQueueEntry, Song}
 import org.slf4j.{Logger, LoggerFactory}
+import de.scalable.database.ScalableDB
+import de.scalable.database.ScalablePostgresProfile.api._
+
+import scala.concurrent.Future
 
 object PartyQueueQueries extends ScalableDB {
 
+  private def insertSongQuery(entry: PartyQueueEntry) =
+    (partyQueueQuery returning partyQueueQuery) += entry
+
   val log: Logger = LoggerFactory.getLogger("PartyQueueQueries")
 
-  private def getSongIDsForKey(partyKey: String) ={
-    partyQueueQuery
+  def getEntriesForParty(partyID: Long): Future[Seq[PartyQueueEntry]] = {
+    val query = partyQueueQuery.filter(_.id === partyID)
+    db.run(query.result)
   }
 
-  def insertSong(song: Song) = {
-
+  def addSongToQueue(songID: Long, partyID: Long): Future[PartyQueueEntry] = {
+    val now = LocalDateTime.now
+    val newEntry = PartyQueueEntry(0L,partyID,songID,0,0,now)
+    db.run(insertSongQuery(newEntry))
   }
+
 }
 
